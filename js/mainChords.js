b5d97abd8cdb8e0096ff5684cb7db9720de00d9c
pedrociarlini <pedrociarlini@gmail.com>
2018-12-13 09:57:40 -0300

    Criação inicial de arquivos

---
+++ js/mainChords.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+function ChordsParser () {
+	this.dados = {
+		grupos : {},
+		repos : {},
+		usuarios : {}
+	};
+
+	this.transpor = function () {
+		// TODO Reconhecer acordes em texto e fazero parse dos mesmos.
+	}
+
+};
