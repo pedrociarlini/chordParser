231cd5e1dd15b3183178cdea19faedd41b3b0245
Simão Gurgel <simao.gurgel@tjce.jus.br>
2019-01-07 12:44:18 -0300

    Falta pouco pra transpor bonito

--- js/mainChords.js
+++ js/mainChords.js
@@ -5,22 +5,36 @@ function ChordsParser () {
 	this.dados = {
 		// lista de uma sequencia que contém uma sequencia de notas
 		// [0] = ["<nota1>", "<nota2>", "<nota3>"]...
-		linhasOriginais : []
+		linhasOriginais : [],
+		textoOriginal : ""
 	};
-	this.notasExistentes = {"C" : 1, "C#": 2, "D": 3, "D#": 4, "E": 5, "F": 6,
-			"F#" : 7, "G" : 8, "G#" : 9, "A" : 10, "A#" : 11, "B" : 12 };
+	this.notasExistentes = {"C" : 1, "C#": 2, "Db": 2, "D": 3, "D#": 4, "Eb": 4, "E": 5, "F": 6,
+			"F#" : 7, "Gb" : 7, "G" : 8, "G#" : 9, "Ab" : 9, "A" : 10, "A#" : 11, "Bb" : 11, "B" : 12 };
+	this.grausExistentes = {};
+
+	for (let nota in this.notasExistentes) {
+		this.grausExistentes[this.notasExistentes[nota]] = nota;
+	}
+	
 
 	/*
 	Transpõe uma nota. O itervalo precisa ser >=-11 <=11.
 	Retorna a nota desejada.
 	*/
-	this.transporNota = function(nota, intervalo) {
+	this.transporNota = function(notaOriginal, intervalo) {
 		if(intervalo < -11 || intervalo > 11) {
 			throw "Intervalo fora do permitido";
 		}
-		if(!this.notasExistentes[nota]) {
-			throw "A nota informada não existe";
+		let nota = notaOriginal.substring(0, 2);
+		if (nota.substring(1, 2) != "#" && nota.substring(1, 2) != "b") {
+			nota = notaOriginal.substring(0, 1)
 		}
+
+		if(!this.notasExistentes[nota]) {
+			throw "A nota informada não existe: " + nota;
+		}
+		// TODO substituições de sustenido (Melhorar isso)
+
 		const grauInicial = this.notasExistentes[nota];
 
 		let grauFinal = grauInicial + intervalo;
@@ -30,25 +44,31 @@ function ChordsParser () {
 			grauFinal += 12;
 		}
 		
-		const result = Object.keys(this.notasExistentes)[grauFinal - 1];
-		// console.log("Nota " + nota + ", transpondo " + intervalo + " fica " + result);
+		let result = this.grausExistentes[grauFinal];
+		// console.log(grauInicial + " = grauInicial | grauFinal = " + (grauFinal - 1) + " " + result);
+
+		result = notaOriginal.replace(nota, result);
+		console.log("Nota " + nota + ", transpondo " + intervalo + " fica " + result);
 		return result;
 	}
 
 	this.tratar = function(texto) {
+		this.dados.textoOriginal = texto;
 		// TODO Reconhecer acordes em texto e fazero parse dos mesmos.
 		// Guardar todos os acordes em sequencia, para cada linha
 		const linhas = texto.split("\n");
+
 		for (let numLinha in linhas) {
-			const linha = linhas[numLinha];
+			let linha = linhas[numLinha];
 			let temNota = false;
 			let acordes = [];
-			console.log("Tratando linha: " + linha);
+			// console.log("Tratando linha: " + linha);
+			linha = linha.replace(/\|/g, " ").trim();
 			const palavras = linha.split(" ");
 			for (let numPal in palavras) {
-				const palavra = palavras[numPal].replace(/\|/g, " ").trim();
+				const palavra = palavras[numPal];
 				if (palavra.trim().length > 0) {
-					console.log("\t> Tratando palavra: [[" + palavra + "]]");
+					// console.log("\t> Tratando palavra: [[" + palavra + "]]");
 					let ehNota = false;
 					for(let nota in this.notasExistentes) {
 						if (palavra.startsWith(nota)) {
@@ -67,18 +87,31 @@ function ChordsParser () {
 			}
 		}
 
-		console.log ("Linhas com acordes: " + this.dados.linhasOriginais.length);
+		// console.log ("Linhas com acordes: " + this.dados.linhasOriginais.length);
 		return this.dados;
 	}
 
-	this.transpor = function(elementSelector) {
-		this.transporNota("E", -5);
+	this.transpor = function(origenSelector, resultSelector, semitonsSelector) {
+		this.tratar($(origenSelector).val());
 
-		const resultPanel = $(elementSelector);
+		const semitons = parseInt($(semitonsSelector).val());
+		console.log(semitons + " semitons")
+		const resultPanel = $(resultSelector);
+		
+		let result = this.dados.textoOriginal;
+		let acordesTranspostos = [];
 		for (let numLinha in this.dados.linhasOriginais) {
 			const linha = this.dados.linhasOriginais[numLinha];
-			resultPanel.append(linha.toString() + "\n");
+			for (let numAcorde in linha) {
+				const acorde = linha[numAcorde];
+				if (!acordesTranspostos.includes(acorde)) {
+					const acordeTransposto = this.transporNota(acorde, semitons);
+					result = result.replace(new RegExp(acorde + " ", 'g'), acordeTransposto + " ");
+					acordesTranspostos.push(acorde);
+				}
+			}
 		}
+		resultPanel.val(result);
 	}
 
 };
