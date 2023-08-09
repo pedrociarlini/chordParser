732af30826dfb9cb60896b9b5cd82d50bce5c2b7
Pedro Ciarlini <pedrociarlini@gmail.com>
2023-08-09 12:12:23 -0300

    Inserindo dados da versão e author acessíveis ao vue.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { defineConfig } = require("@vue/cli-service");
+const { version, displayName, author } = require("./package");
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false
-})
+    transpileDependencies: true,
+    lintOnSave: false,
+    pages: {
+        index: {
+            entry: "src/main.ts",
+            title: displayName,
+            version: version,
+            author: author.name,
+        },
+    },
+});
